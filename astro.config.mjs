507cdaaa6fc47c847c1e72f5ee0f4a442faf7c49
pwh-pwh <plmoknpwh@163.com>
2024-01-22 15:46:20 +0800

    add simple preact component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://coderpwhblog.netlify.app"
+    site: "https://coderpwhblog.netlify.app",
+    integrations: [preact()]
 });
