2b36619d140b633b7d4d37c4e624a2842580bdd7
pwh-pwh <plmoknpwh@163.com>
2024-01-24 20:50:52 +0800

    change theme

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,21 +1,26 @@
-// Import utilities from `astro:content`
-import { z, defineCollection } from "astro:content";
-// Define a `type` and `schema` for each collection
-const postsCollection = defineCollection({
-    type: 'content',
-    schema: z.object({
-        title: z.string(),
-        pubDate: z.date(),
-        description: z.string(),
-        author: z.string(),
-        image: z.object({
-            url: z.string(),
-            alt: z.string()
-        }),
-        tags: z.array(z.string())
-    })
+import { SITE } from "@config";
+import { defineCollection, z } from "astro:content";
+
+const blog = defineCollection({
+  type: "content",
+  schema: ({ image }) =>
+    z.object({
+      author: z.string().default(SITE.author),
+      pubDatetime: z.date(),
+      modDatetime: z.date().optional().nullable(),
+      title: z.string(),
+      featured: z.boolean().optional(),
+      draft: z.boolean().optional(),
+      tags: z.array(z.string()).default(["others"]),
+      ogImage: image()
+        .refine(img => img.width >= 1200 && img.height >= 630, {
+          message: "OpenGraph image must be at least 1200 X 630 pixels!",
+        })
+        .or(z.string())
+        .optional(),
+      description: z.string(),
+      canonicalURL: z.string().optional(),
+    }),
 });
-// Export a single `collections` object to register your collection(s)
-export const collections = {
-    posts: postsCollection,
-};
+
+export const collections = { blog };
---
+++ src/pages/about.md
@@ -0,0 +1,30 @@
+---
+layout: ../layouts/AboutLayout.astro
+title: "About"
+---
+
+My name is coderpwh.
+
+I am interested in web development.I want to learn web3.
+
+Here are a few facts about me.
+
+<div>
+  <img src="/assets/dev.svg" class="sm:w-1/2 mx-auto" alt="coding dev illustration">
+</div>
+
+## Hobbies
+
+This theme is written in vanilla JavaScript (+ TypeScript for type checking) and a little bit of ReactJS for some interactions. TailwindCSS is used for styling; and Markdown is used for blog contents.
+
+## Skills
+
+Here are some of my skills.
+
+- rust
+- kotlin
+- web
+- xposed
+- golang
+
+If you want to contact me, please click on the social platform below to contact me。
